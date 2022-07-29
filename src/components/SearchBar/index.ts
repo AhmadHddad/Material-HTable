78125f8e6b2356c1e28e3edd21fbd7c255522a3d
AhmadHddad <AhmadHddad@outlook.com>
2022-07-29 13:04:56 +0300

    setting default props

--- src/components/SearchBar/index.ts
+++ src/components/SearchBar/index.ts
@@ -3,16 +3,16 @@ import {GridProps} from "@mui/system";
 import {IColor, IHComponents} from "components/HTable";
 
 export type SearchBarProps = {
-    searchOptions: {
-        searchInputProps: TextFieldProps;
-        clearBtnProps: (searchText: string) => IconButtonProps | IconButtonProps;
-        searchPlaceholder: string;
-        searchInputContainerProps: GridProps;
-        searchBtnProps: (searchText: string) => IconButtonProps | IconButtonProps;
-        onClearSearch: (searchText: string) => void;
-        onSearchClick: (searchText: string) => void;
+    searchOptions?: {
+        searchInputProps?: TextFieldProps;
+        clearBtnProps?: (searchText: string) => IconButtonProps | IconButtonProps;
+        searchPlaceholder?: string;
+        searchInputContainerProps?: GridProps;
+        searchBtnProps?: (searchText: string) => IconButtonProps | IconButtonProps;
+        onClearSearch?: (searchText: string) => void;
+        onSearchClick?: (searchText: string) => void;
     };
-    color: IColor;
+    color?: IColor;
     searchText: string;
     setSearchTextValue: (searchText: string) => void;
     components: IHComponents;
