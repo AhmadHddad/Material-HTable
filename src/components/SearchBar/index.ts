ae4eb905a90c6d2bf03e9b5f34ff834db40205bf
AhmadHddad <AhmadHddad@outlook.com>
2022-07-29 12:56:03 +0300

    adding SearchBarProps

---
+++ src/components/SearchBar/index.ts
@@ -0,0 +1,19 @@
+import {IconButtonProps, TextFieldProps} from "@mui/material";
+import {GridProps} from "@mui/system";
+import {IColor, IHComponents} from "components/HTable";
+
+export type SearchBarProps = {
+    searchOptions: {
+        searchInputProps: TextFieldProps;
+        clearBtnProps: (searchText: string) => IconButtonProps | IconButtonProps;
+        searchPlaceholder: string;
+        searchInputContainerProps: GridProps;
+        searchBtnProps: (searchText: string) => IconButtonProps | IconButtonProps;
+        onClearSearch: (searchText: string) => void;
+        onSearchClick: (searchText: string) => void;
+    };
+    color: IColor;
+    searchText: string;
+    setSearchTextValue: (searchText: string) => void;
+    components: IHComponents;
+};
