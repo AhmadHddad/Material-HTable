8f4269a9138e502691a2efb043dbd8057e3a0e86
AhmadHddad <AhmadHddad@outlook.com>
2022-07-29 12:14:51 +0300

    passing required props

--- src/components/HTable/HTable.js
+++ src/components/HTable/HTable.js
@@ -1,46 +1,78 @@
 import "./hTableSyle.css";
-import { useState } from "react";
 import * as React from "react";
 import {
-    Grid,
-    TextField,
-    IconButton,
-    CardContent,
-    TableContainer,
-    TablePagination,
-    Card
+    TableContainer as MTableContainer,
+    TablePagination as MTablePagination,
+    Table as MTalbe,
+    IconButton as MIconButton,
+    TextField as MTextField,
+    TableRow as MTableRow,
+    TableCell as MTableCell,
+    Checkbox as MCheckbox,
+    TableSortLabel as MTableSortLabel,
+    TableHead as MTableHead,
+    TableBody as MTableBody,
+    Grid as MGrid,
+    Card as MCard,
+    Collapse,
 } from "@mui/material";
-import PropTypes from "prop-types";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
 import ClearIcon from "@mui/icons-material/Clear";
 import SearchIcon from "@mui/icons-material/Search";
+import PropTypes from "prop-types";
 import classNames from "classnames";
-import { returnObjFromFunc, toObject, uuidv4 } from "../../utils";
+import { conditionalReturn, returnObjFromFunc, toArray, toObject } from "../../utils";
 import HTableHead from "components/HTableHead/HTableHead";
 import HTableEmptyView from "components/HTableEmptyView/HTableEmptyView";
 import HTableLoadingView from "components/HTableLoadingView/HTableLoadingView";
 import useHTableStyle from "./hTableStyle";
 import HTableRow from "components/HTableRow/HTableRow";
+import SearchBar from "components/SearchBar/SearchBar";
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
 const headCells = [
     {
         id: "name",
-        numeric: false,
+        align: "left",
         disablePadding: true,
-        label: "Dessert (100g serving)"
+        props: {},
+        sortLabelProps: {},
+        component: "Dessert (100g serving)"
     },
     {
         id: "calories",
         numeric: true,
         disablePadding: true,
-        label: "Calories"
+        component: "Calories"
     },
     {
         id: "collapseRow",
-        label: ""
+        component: ""
     }
 ];
+
+
+
+export const allComponents = {
+    Table: MTalbe,
+    Card: MCard,
+    TableContainer: MTableContainer,
+    TablePagination: MTablePagination,
+    TableBody: MTableBody,
+    TextField: MTextField,
+    IconButton: MIconButton,
+    TableRow: MTableRow,
+    KeyboardArrowUpIcon,
+    KeyboardArrowDownIcon,
+    TableHead: MTableHead,
+    TableCell: MTableCell,
+    Grid: MGrid,
+    SearchIcon,
+    ClearIcon,
+    Collapse,
+    Checkbox: MCheckbox,
+    TableSortLabel: MTableSortLabel,
+};
 
 function HTable({
     heads,
@@ -54,27 +86,31 @@ function HTable({
     disabled,
     tableContainerProps,
     tableBodyprops,
+    sortable,
     addPagination,
+    collapseOptions,
     headOptions,
-    search,
+    searchable,
     emptyView,
+    tableProps,
+    color,
     searchOptions,
-    paginationOptions
+    paginationOptions,
+    onSort,
+    orderBy,
+    order,
+    cardProps,
+    onSelectAllClick,
+    onRowSelected
 }) {
     const props = arguments[0];
     const classes = useHTableStyle(props);
-    const { headCellProps } = toObject(headOptions);
-    const {
-        defaultSearchText,
-        searchInputProps,
-        clearBtnProps,
-        searchIcon,
-        searchInputContainerProps,
-        searchBtnProps,
-        onClearSearch,
-        onSearchClick
-    } = toObject(searchOptions);
-    const { } = rowOptions;
+
+    const { defaultSelectedIds } = toObject(selectOptoins)
+    const { defaultSearchText } = toObject(searchOptions);
+    const components = Object.assign(allComponents, props.components);
+    const { Card, TableContainer, Table, TableBody, TablePagination } = components;
+
     const {
         rowsPerPageOptions,
         totalItems,
@@ -82,30 +118,33 @@ function HTable({
         pageNumber,
         onRowsPerPageChange,
         onPageChange,
-        classes: paginationClasses,
-        component: paginationComponent
+        component: paginationComponent,
+        paginationProps
     } = toObject(paginationOptions);
 
-    const [searchText, setSearchTextValue] = useState(defaultSearchText || "");
-    const [selected, setSelected] = React.useState([]);
+    const [searchText, setSearchTextValue] = React.useState(defaultSearchText || "");
+    const [selected, setSelected] = React.useState(toArray(defaultSelectedIds));
 
     const totalCount = totalItems || rows?.length;
 
     const handleSelectAllClick = event => {
+        let updatedSelected = [];
+
         if (event.target.checked) {
-            const newSelecteds = rows.map(n => n.id);
-            setSelected(newSelecteds);
-            return;
+            updatedSelected = rows.map(n => n.id);
         }
-        setSelected([]);
+
+        setSelected(updatedSelected);
+        onSelectAllClick?.(event, updatedSelected)
     };
 
-    const handleClick = (event, name) => {
-        const selectedIndex = selected.indexOf(name);
+    const handleClick = (event, row) => {
+        const rowId = row.id;
+        const selectedIndex = selected.indexOf(rowId);
         let newSelected = [];
 
         if (selectedIndex === -1) {
-            newSelected = newSelected.concat(selected, name);
+            newSelected = newSelected.concat(selected, rowId);
         } else if (selectedIndex === 0) {
             newSelected = newSelected.concat(selected.slice(1));
         } else if (selectedIndex === selected.length - 1) {
@@ -118,73 +157,17 @@ function HTable({
         }
 
         setSelected(newSelected);
+        onRowSelected?.(event, newSelected)
     };
 
-    const SearchIcn = searchIcon || <SearchIcon fontSize="small" />;
-    function renderSearchBar() {
-        if (!search) return null;
-
-        return (
-            <form
-                onSubmit={e => {
-                    e.preventDefault();
-                    onSearchClick?.(searchText);
-                }}
-            >
-                <Grid
-                    container
-                    item
-                    padding={1}
-                    justifyContent={"flex-start"}
-                    {...searchInputContainerProps}
-                >
-                    <TextField
-                        variant="standard"
-                        onChange={e => setSearchTextValue(e.target.value)}
-                        InputProps={{
-                            startAdornment: (
-                                <IconButton
-                                    title="Clear"
-                                    type="submit"
-                                    aria-label="Clear"
-                                    size="small"
-                                    onClick={function () {
-                                        onSearchClick?.(searchText);
-                                    }}
-                                    {...returnObjFromFunc(searchBtnProps, searchText)}
-                                >
-                                    {SearchIcn}
-                                </IconButton>
-                            ),
-                            endAdornment: (
-                                <IconButton
-                                    title="Clear"
-                                    aria-label="Clear"
-                                    size="small"
-                                    onClick={() => {
-                                        setSearchTextValue("");
-                                        onClearSearch?.();
-                                    }}
-                                    style={{ visibility: searchText ? "visible" : "hidden" }}
-                                    {...returnObjFromFunc(clearBtnProps, searchText)}
-                                >
-                                    <ClearIcon fontSize="small" />
-                                </IconButton>
-                            )
-                        }}
-                        {...returnObjFromFunc(searchInputProps)}
-                    />
-                </Grid>
-            </form>
-        );
-    }
+    const searchBar = conditionalReturn(searchable, <SearchBar components={components} searchOptions={setSearchTextValue} searchText={searchText} setSearchTextValue={setSearchTextValue} />)
 
     const renderLoaderOrEmptyView = () => {
         const LoaderView = loaderView || <HTableLoadingView />;
         const EmptyView = emptyView || <HTableEmptyView emptyViewText={emptyViewText} />;
         return (
             <>
-                {renderSearchBar()}
+                {searchBar}
                 {isLoading ? LoaderView : EmptyView}
             </>
         );
@@ -192,93 +175,103 @@ function HTable({
 
     const isSelected = id => selected.indexOf(id) !== -1;
 
+    function onRef(ref) {
+        disabled && ref && ref.addEventListener("keydown", event => {
+            event.preventDefault();
+        })
+    }
+
     return (
-        <Card>
-            <CardContent className={classNames(classes.cardBody)}>
-                {isLoading || !rows?.length ? (
-                    renderLoaderOrEmptyView()
-                ) : (
-                    <>
-                        {renderSearchBar()}
-                        <TableContainer
-                            {...returnObjFromFunc(tableContainerProps)}
-                            component={"div"}
-                        >
-                            <Table className={classes.table} aria-label="simple table">
-                                <HTableHead
-                                    numSelected={selected?.length}
-                                    onSelectAllClick={handleSelectAllClick}
-                                    heads={heads}
-                                    rowCount={totalCount}
-                                    selectable={selectable}
-                                />
-                                <TableBody {...toObject(tableBodyprops)}>
-                                    {rows.map((row, index) => {
-                                        const isRowSelected = isSelected(row.id);
-                                        return (
-                                            <HTableRow
-                                                selectOptoins={selectOptoins}
-                                                selectable={selectable}
-                                                index={index}
-                                                key={row.id || index}
-                                                row={row}
-                                                onRowClicked={event => handleClick(event, row.id)}
-                                                isRowSelected={isRowSelected}
-                                            />
-                                        );
-                                    })}
-                                </TableBody>
-                            </Table>
-                        </TableContainer>
-                        {addPagination && (
-                            <TablePagination
-                                component={paginationComponent || "div"}
-                                rowsPerPageOptions={rowsPerPageOptions}
-                                classes={
-                                    paginationClasses || {
-                                        root: classes.root
-                                    }
-                                }
-                                count={totalCount}
-                                rowsPerPage={rowsPerPage}
-                                page={pageNumber - 1}
-                                onPageChange={(...args) => onPageChange(...args, searchText)}
-                                onRowsPerPageChange={(...args) =>
-                                    onRowsPerPageChange(...args, searchText)
-                                }
-                            />
-                        )}
-                    </>
-                )}
-            </CardContent>
-        </Card>
+        <TableContainer
+            className={classNames({ [classes.disabled]: disabled })} ref={onRef} {...toObject(cardProps)}
+            component={Card}
+            {...returnObjFromFunc(tableContainerProps)}
+        >
+            {isLoading || !rows?.length ? (
+                renderLoaderOrEmptyView()
+            ) : (
+                <>
+                    {searchBar}
+                    <Table  {...toObject(tableProps)}>
+                        <HTableHead
+                            //color, components, heads, numSelected, headRowProps,selectAllOptions headProps, rowCount, , onSelectAllClick, sortable, onSort, orderBy, order, selectable
+                            components={components}
+                            numSelected={selected?.length ?? 0}
+                            color={color}
+                            onSelectAllClick={handleSelectAllClick}
+                            heads={heads}
+                            sortable={sortable}
+                            order={order}
+                            orderBy={orderBy}
+                            onSort={onSort}
+                            rowCount={totalCount ?? 0}
+                            selectable={selectable}
+                            {...toObject(headOptions)}
+                        />
+                        <TableBody {...toObject(tableBodyprops)}>
+                            {rows.map((row, index) => {
+                                const isRowSelected = isSelected(row.id);
+                                return (
+                                    <HTableRow
+                                        rowOptions={rowOptions}
+                                        components={components}
+                                        selectOptoins={selectOptoins}
+                                        collapseOptions={collapseOptions}
+                                        color={color}
+                                        selectable={selectable}
+                                        index={index}
+                                        key={row.id || index}
+                                        row={row}
+                                        onRowClicked={handleClick}
+                                        isRowSelected={isRowSelected}
+                                    />
+                                );
+                            })}
+                        </TableBody>
+                    </Table>
+                </>)}
+            {addPagination && (
+                <TablePagination
+                    component={paginationComponent || "div"}
+                    rowsPerPageOptions={rowsPerPageOptions}
+                    count={totalCount}
+                    rowsPerPage={rowsPerPage}
+                    page={pageNumber}
+                    onPageChange={(...args) => onPageChange(...args, searchText)}
+                    onRowsPerPageChange={(...args) =>
+                        onRowsPerPageChange(...args, searchText)
+                    }
+                    {...toObject(paginationProps)}
+                />
+            )}
+        </TableContainer>
     );
 }
 
 HTable.defaultProps = {
     heads: headCells,
     selectable: true,
-    rows: new Array(5)
-        .fill({
-            id: 1,
-            cells: [
-                { id: "cellId1", component: "Row#1Cell1", props: {} },
-                { id: "cellId2", component: "Row#1Cell2", props: {} }
-            ],
-            props: {},
-            collapseRow: { component: <div>HEllo</div>, props: {} }
-        })
-        .map(o => ({ ...o, id: uuidv4() })),
+    rows: [{
+        id: 1,
+        cells: [
+            { id: "cellId1", component: "Row#1Cell1", props: {} },
+            { id: "cellId2", component: "Row#1Cell2", props: {} }
+        ],
+        props: {},
+        collapseRow: { component: <div>Collapse Row</div>, rowPorps: {}, cellProps: {} }
+    }],
     rowOptions: {},
     isLoading: false,
-    disabled: false,
     tableContainerProps: {},
     hover: false,
     addPagination: false,
+    color: "primary",
     headersOptions: {},
-    search: false,
+    disabled: false,
+    searchable: true,
     emptyView: null,
     searchOptions: {},
+    components: {},
     paginationOptions: {}
 };
 
@@ -295,7 +288,7 @@ HTable.propTypes = {
     select: PropTypes.bool,
     checkbox: PropTypes.bool,
     paperEffect: PropTypes.bool,
-    search: PropTypes.bool,
+    searchable: PropTypes.bool,
     rowsPerPageOptions: PropTypes.array,
     searchInputProps: PropTypes.object,
     searchInputContainerProps: PropTypes.object,
@@ -308,6 +301,8 @@ HTable.propTypes = {
     onRowClick: PropTypes.func,
     headerIcon: PropTypes.any,
     headerTitle: PropTypes.string,
+    color: PropTypes.string,
+    components: PropTypes.object,
     onChangeRowsPerPage: PropTypes.func
 };
 
