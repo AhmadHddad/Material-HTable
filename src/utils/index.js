c99f202fb8a39dafcc999f5c8f0d3a475fc67c14
AhmadHddad <AhmadHddad@outlook.com>
2022-07-29 15:58:51 +0300

    removing unessary util

--- src/utils/index.js
+++ src/utils/index.js
@@ -17,11 +17,3 @@ export const conditionalReturn = (condition, first, second = null) => condition 
 export const callIfFunctionOrReturnIfObj = (source, param) => typeof source === 'function' ? source(param) : toObject(source)
 export const returnObjFromFunc = (source, param) => toObject(callIfFunctionOrReturnIfObj(source, param))
 
-
-export function uuidv4() {
-    //@ts-ignore
-    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
-      (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16)
-    );
-  }
-  
