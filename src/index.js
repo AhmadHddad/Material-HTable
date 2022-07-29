ffd59778aa9f77378557e9ac13a4fe51eebbd705
AhmadHddad <AhmadHddad@outlook.com>
2022-07-30 01:46:05 +0300

    updating index with default exports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import HTable from 'components/HTable/HTable';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <HTable />
-  </React.StrictMode>
-);
+
+export default HTable;
+export * from "./components";
