22800b1d299bc8bcbc52dd28efc89ca07e4cfa99
AhmadHddad <AhmadHddad@outlook.com>
2022-07-29 17:47:08 +0300

    removing app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
+import HTable from 'components/HTable/HTable';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HTable />
   </React.StrictMode>
 );
