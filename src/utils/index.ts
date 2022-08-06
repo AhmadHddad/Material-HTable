bb9ef26a22336437810208d3eae0962f6efe2c33
AhmadHddad <AhmadHddad@outlook.com>
2022-08-06 20:29:02 +0300

    using classnames instead

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -31,38 +31,3 @@ export function returnObjFromFunc<T = any>(source?: any, param?: T) {
 export function callFuncOrReturn<T = any>(first: T, ...rest: any) {
   return typeof first === 'function' ? first(...rest) : first;
 }
-
-export function classNames(..._: any) {
-  var classes: any[] = [];
-  const hasOwn = {}.hasOwnProperty;
-
-  for (var i = 0; i < arguments.length; i++) {
-    var arg: any = arguments[i];
-    if (!arg) continue;
-
-    var argType = typeof arg;
-
-    if (argType === 'string' || argType === 'number') {
-      classes.push(arg);
-    } else if (Array.isArray(arg)) {
-      if (arg.length) {
-        var inner = classNames.apply(null, arg as any);
-        if (inner) {
-          classes.push(inner);
-        }
-      }
-    } else if (argType === 'object') {
-      if (arg.toString === Object.prototype.toString) {
-        for (var key in arg as []) {
-          if (hasOwn.call(arg, key) && arg[key]) {
-            classes.push(key);
-          }
-        }
-      } else {
-        classes.push(arg.toString());
-      }
-    }
-  }
-
-  return classes.join(' ');
-}
